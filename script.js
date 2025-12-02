a7fc4fae958353f52e6d4d0351afecbaebd56873
Nora Rice <nora.rice09@gmail.com>
2025-12-02 14:30:03 -0700

    Testing website color button.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = "lightblue";
+}
 
-          <button onclick="changeBackgroundColor()">Change Background Color</button>
+<button onclick="changeBackgroundColor()">Change Background Color</button>
+          
 
-        function playBackgroundMusic() {
-            var audio = new Audio('jazz-lounge-elevator-music-332339.mp3');
-            audio.loop = true;
-            audio.play();
+function playBackgroundMusic() {
+    var audio = new Audio('jazz-lounge-elevator-music-332339.mp3');
+    audio.loop = true;
+    audio.play();
         }
         // Modify your window.onload function
         window.onload = function() {
