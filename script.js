7466d384f30870ecf513de468a1569a353ecb1cb
Nora Rice <nora.rice09@gmail.com>
2025-12-02 14:19:28 -0700

    redo with another file.

---
+++ script.js
@@ -0,0 +1,12 @@
+
+          <button onclick="changeBackgroundColor()">Change Background Color</button>
+
+        function playBackgroundMusic() {
+            var audio = new Audio('jazz-lounge-elevator-music-332339.mp3');
+            audio.loop = true;
+            audio.play();
+        }
+        // Modify your window.onload function
+        window.onload = function() {
+            playBackgroundMusic();
+};
