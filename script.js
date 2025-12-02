eaf8cbb46d64d794f174ad6674b98ed657a18624
Nora Rice <nora.rice09@gmail.com>
2025-12-02 14:45:18 -0700

    another test for the background color button on page 2.

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-function changeBackgroundColor(BackgroundColorButton) {
+function changeBackgroundColor() {
     document.body.style.backgroundColor = "lightblue";
 }
 
 <button onclick="changeBackgroundColor()">Change Background Color</button>
-          
 
-function playBackgroundMusic() {
-    var audio = new Audio('jazz-lounge-elevator-music-332339.mp3');
-    audio.loop = true;
-    audio.play();
-        }
+///function playBackgroundMusic() {
+   // var audio = new Audio('jazz-lounge-elevator-music-332339.mp3');
+   // audio.loop = true;
+   // audio.play();
+   //     }
         // Modify your window.onload function
-        window.onload = function() {
-            playBackgroundMusic();
-};
+    //    window.onload = function() {
+    //        playBackgroundMusic();};
